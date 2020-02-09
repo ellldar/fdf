22909e48350a8c68e4030e6fbe9922e90bf162e8
esupatae <esupatae@student.42.us.org>
2020-02-08 16:01:30 -0800

    Attempting to refine anti-aliasing

--- src/draw_ops.c
+++ src/draw_ops.c
@@ -29,6 +29,24 @@ void		put_pixel(t_scope *scope, int x, int y, int color)
 	*(unsigned int*)addr = color;
 }
 
+static void	draw_box(t_scope *scope, int x, int y, int n, int color)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < 100)
+	{
+		j = 0;
+		while (j < 100)
+		{
+			put_pixel(scope, n * 100 + x + i, y + j, color);
+			j++;
+		}
+		i++;
+	}
+}
+
 void		draw_line(t_scope *scope, int x, int y, int x1, int y1, int color)
 {
 	t_line	*line;
@@ -41,7 +59,13 @@ void		draw_line(t_scope *scope, int x, int y, int x1, int y1, int color)
 	while (i <= line->step)
 	{
 		calc_antialias(line, color);
-		if (line->ratio != 0.0 && !is_endpoint(line, x, y, x1, y1))
+		if (i >= 10 && i < 20)
+		{
+			draw_box(scope, 100, 100, i - 10, line->alpha);
+			draw_box(scope, 100, 200, i - 10, line->reverse_alpha);
+			printf("ratio: %.3f, alpha: %i\n", line->ratio, line->alpha);
+		}
+		if (line->alpha != color && !is_endpoint(line, x, y, x1, y1))
 		{
 			put_pixel(scope, round(line->x), round(line->y), line->alpha);
 			put_second_pixel(scope);
@@ -71,9 +95,17 @@ static void	calc_antialias(t_line *var, int color)
 		var->ratio = var->y - (float)round(var->y);
 	else
 		var->ratio = var->x - (float)round(var->x);
-	var->alpha = (int)(round(0x7F * fabs(var->ratio / 0.5)));
-	var->reverse_alpha = ((0xFF - var->alpha) << 24) + color;
-	var->alpha = (var->alpha << 24) + color;
+	if (fabsf(var->ratio) > 0.02)
+	{
+		var->alpha = (int)(round(0x7F * fabs(var->ratio / 0.5)));
+		var->reverse_alpha = ((0xFF - var->alpha) << 24) + color;
+		var->alpha = (var->alpha << 24) + color;
+	}
+	else
+	{
+		var->alpha = color;
+		var->reverse_alpha = 0;
+	}
 }
 
 static void	put_second_pixel(t_scope *scope)
